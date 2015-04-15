3c71aeb704580d68fad33e34a46d876bda6df46d
Jonathan D. Tang <jontang@princeton.edu>
2015-04-15 16:27:49 -0400

    Random content changes

--- controllers/process.js
+++ controllers/process.js
@@ -39,6 +39,37 @@ exports.objectiveHeuristics = function(text) {
 /* Wordnik: http://videlais.com/2015/03/25/starting-with-the-wordnik-api-in-node-js/ */
 /* API Key: 8f7a98ece2c502050b0070ea7420d05f07b7e17ec1aca1b27 */
 
+exports.getWordnik = function(word) {
+
+}
+
+/*
+
+exports.getEtymologyOrg = function(word) {
+ 	var url = 'http://www.etymonline.com/index.php?search=';
+ 	var full_url = url.concat(word);
+
+    // The structure of our request call
+    // The first parameter is our URL
+    // The callback function takes 3 parameters, an error, response status code and the html
+
+    request(url, function(error, response, html){
+
+        // First we'll check to make sure no errors occurred when making the request
+
+        if(!error){
+            // Next, we'll utilize the cheerio library on the returned html which will essentially give us jQuery functionality
+
+            //var $ = cheerio.load(html);
+
+            // Finally, we'll define the variables we're going to capture
+
+            //var title, release, rating;
+            //var json = { title : "", release : "", rating : ""};
+        }
+    })
+}/*/
+
 exports.subjectiveHeuristics = function(text) {
 
 	var APIKEY = '8f7a98ece2c502050b0070ea7420d05f07b7e17ec1aca1b27';
