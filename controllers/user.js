cae52e419c83a31bf7d1ecf7ea554d33c9baa2ce
Diana Liao <dl.amorphous@gmail.com>
2015-05-06 13:48:55 -0400

    Change the email used to send password recoveries

--- controllers/user.js
+++ controllers/user.js
@@ -265,7 +265,7 @@ exports.postReset = function(req, res, next) {
       });
       var mailOptions = {
         to: user.email,
-        from: 'hackathon@starter.com',
+        from: 'jontang@princeton.edu',
         subject: 'Lexica Reader: Your password has been changed.',
         text: 'Hello,\n\n' +
           'This is a confirmation that the password for your account ' + user.email + ' has just been changed.\n'
@@ -340,7 +340,7 @@ exports.postForgot = function(req, res, next) {
       });
       var mailOptions = {
         to: user.email,
-        from: 'hackathon@starter.com',
+        from: 'jontang@princeton.edu',
         subject: 'Reset your password on Lexica Reader',
         text: 'You are receiving this email because you (or someone else) have requested the reset of the password for your account.\n\n' +
           'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
